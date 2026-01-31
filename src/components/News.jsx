2c6e9438e1078267db33f9b46f85820b0108bc43
Rinkal Koshti <rinkalkoshti2004@gmail.com>
2026-01-31 17:57:01 +0530

    displayed cards

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,72 +1,93 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
-
   constructor() {
     super();
     this.state = {
       articles: [],
-      loading: false,
-      page: 1
-    }
+      page: 1,
+      totalResults: 0,
+    };
+  }
+
+  fetchNews = async (page) => {
+    console.log("Fetch called");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc5029fa80c446fc9d957ee7b41ab7b9&page=${page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    console.log("Page number:", page);
+    console.log("Page Size:", this.props.pageSize);
+    this.setState({
+      articles: parsedData.articles || [],
+      totalResults: parsedData.totalResults || 0,
+      page: page, 
+    });
   }
 
   async componentDidMount() {
     // API call can be made here to fetch real news data
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc5029fa80c446fc9d957ee7b41ab7b9&page=1&pageSize=20";
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+    this.fetchNews(1)
   }
 
   handlePrevClick = async () => {
-    console.log("Previous");
-     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc5029fa80c446fc9d957ee7b41ab7b9&page=${this.state.page - 1}&pageSize=20`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({ 
-      page: this.state.page - 1 ,
-      articles: parsedData.articles
-    });
-  }
+    if(this.state.page > 1) {
+      const prevPage = this.state.page - 1;
+      this.setState({page: prevPage});
+      console.log("Previous");
+      this.fetchNews(prevPage);
+    }
+  };
 
   handleNextClick = async () => {
     console.log("Next");
-    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)) {
-
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc5029fa80c446fc9d957ee7b41ab7b9&page=${this.state.page + 1}&pageSize=20`;
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      console.log(parsedData);
-      this.setState({ 
-        page: this.state.page + 1,
-        articles: parsedData.articles
-      });
+    const totalPages = Math.ceil(this.state.totalResults / this.props.pageSize);
+    if(this.state.page < totalPages) {
+      const nextPage = this.state.page + 1;
+      this.setState({page: nextPage});
+      this.fetchNews(nextPage);
     }
-  }
+  };
 
   render() {
     return (
       <>
-      <div className='w-full overflow-x-hidden my-7 px-30' >
-      <h1 className='text-2xl font-medium mb-5'>NewsMonkey - Top Headlines</h1>
-      <div className='flex flex-wrap gap-20 justify-center' >
-      {this.state.articles.map((element) => {
-        return <NewsItem key={element.url} title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
-      })}
-      </div>
-      <div className='flex justify-between mt-2'>
-        <button disabled={this.state.page <= 1} className="bg-red-600 px-4 py-2 rounded-3xl text-white mt-2" onClick={this.handlePrevClick}>&larr; Previous</button>
-        <button className="bg-red-600 px-4 py-2 rounded-3xl text-white mt-2" onClick={this.handleNextClick}>Next &rarr;</button>
-      </div>
-      </div>
-    </>
-    )
+        <div className="w-full overflow-x-hidden my-7 px-30">
+          <h1 className="text-2xl font-medium mb-5 text-center">
+            NewsMonkey - Top Headlines
+          </h1>
+          <div className="w-full mx-auto flex flex-wrap gap-15 justify-center items-center">
+            {this.state.articles.map((element, index) => {
+                return (<NewsItem
+                  key={element.url || index}
+                  title={element.title}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />);
+            })}
+          </div>
+          <div className="flex justify-between mt-2">
+            <button
+              disabled={this.state.page <= 1}
+              className="bg-red-600 px-4 py-2 rounded-3xl text-white mt-2"
+              onClick={this.handlePrevClick}
+            >
+              &larr;
+            </button>
+            <button
+              disabled={this.state.page >= Math.ceil(this.state.totalResults / this.props.pageSize)}
+              className="bg-red-600 px-4 py-2 rounded-3xl text-white mt-2"
+              onClick={this.handleNextClick}
+            >
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -5,17 +5,15 @@ export class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <>
-        <div className="card w-90 p-4 shadow-xl">
-          <img src={imageUrl} alt="book image" className="m-auto object-cover"
-          onError={(e) => {
-            e.target.src = "https://wpengine.com/wp-content/uploads/2021/05/optimize-images-1024x681.jpg"
-          }} />
-          <div className="px-3">
-            <h3 className="font-bold my-3">{title}...</h3>
-            <p className="mb-5">{description}...</p>
+        <div className="card w-90 h-110 p-4 shadow-xl">
+          <img src= {imageUrl ? imageUrl : "https://wpengine.com/wp-content/uploads/2021/05/optimize-images-1024x681.jpg"}
+          alt={title ? title : "book image"} className="m-auto object-cover w-100 h-50" />
+          <div className="px-3 relative">
+            <h3 className="font-bold my-3 w-full h-10">{title ? title.slice(0, 55) + "..." : "No title available"}</h3>
+            <p className="mb-5 w-full h-fit">{description ? description.slice(0, 105) + "..." : "No description available"}</p>
             <a
               href={newsUrl} target="_blank"
-              className="bg-red-600 px-4 py-2 rounded-3xl text-white mt-2"
+              className="bg-red-600 px-4 py-2 rounded-3xl text-white absolute right-0"
             >
               Read More
             </a>
